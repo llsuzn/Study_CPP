491b52a6677126df0841dcb963fcb3c655fccab7
llsuzn <dltnwls0987522@gmail.com>
2023-03-21 17:20:06 +0900

    3.21

--- base_Cpp/11_inline.cpp
+++ base_Cpp/11_inline.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
-
+// 템플릿 함수로 보완한 인라인 함수
 using namespace std;
 
-inline int SQUARE(int x)
+template <typename T>
+inline T SQUARE(T x)
 {
 	return x * x;
 }
 
 int main() {
-	cout << SQUARE(6) << endl;
+	cout << SQUARE(6.6) << endl;
 
 	return 0;
 }
