0d6b2c98572b1fb1ad9e32e1fccc75a56afea30d
llsuzn <dltnwls0987522@gmail.com>
2023-03-24 17:29:48 +0900

    3.24

--- base_Cpp/46_compile_initializer.cpp
+++ base_Cpp/46_compile_initializer.cpp
@@ -7,16 +7,17 @@ class Myclass
 private:
 	int age;
 	char id;
-	char name[10];
+	char * name;
 
 	//멤버함수(=메소드): 기능
 public:
 	Myclass(int mage, char mid, char* mname); // 생성자 원형
-	void getData(); // 메소드 원형(선언)
+	void getData(); // 메소드 원형(선언)	
 };
 
 Myclass::Myclass(int mage, char mid, char* mname) :age(mage), id(mid) // 생성자 정의, 멤버 이니셜라이저를 이용한 멤버 초기화
 {
+	name = new char[10];
 	strcpy(name, mname);
 }
 
